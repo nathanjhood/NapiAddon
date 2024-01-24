c9b955d6d79133d759e01f032d24c6cc9692eef9
StoneyDSP <nathanjhood@googlemail.com>
2024-01-24 01:26:46 +0100

    Improved tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,32 @@
-const addon = require("../lib/addon.node");
+function test() {
 
-console.log(addon.hello());
-console.log(`Napi Version: ${addon.version()}`);
+  try {
+
+    const addon = require("../lib/addon.node");
+
+    console.log(addon.hello());
+    console.log(`Napi Version: ${addon.version()}`);
+
+  } catch(e) {
+
+    console.log(`Error: ${e}`);
+  }
+};
+
+function test_v7() {
+
+  try {
+
+    const addon_v7 = require("../build/lib/addon_v7.node");
+
+    console.log(addon_v7.hello());
+    console.log(`Napi Version: ${addon_v7.version()}`);
+
+  } catch(e) {
+
+    console.log(`Error: ${e}`);
+  }
+};
+
+test();
+test_v7();
